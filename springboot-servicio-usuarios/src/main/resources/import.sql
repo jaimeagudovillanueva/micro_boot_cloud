d084df35972eedd18520d7517b84966da7de20cf
Jaime Agudo Villanueva <jaimeagudovillanueva@gmail.com>
2020-03-16 16:03:33 +0100

    Añadido postresql en servicio-usuarios

--- springboot-servicio-usuarios/src/main/resources/import.sql
+++ springboot-servicio-usuarios/src/main/resources/import.sql
@@ -1,5 +1,5 @@
-INSERT INTO usuarios (username, password, enabled, nombre, apellido, email) VALUES ('jaime.agudo','$2a$10$ykhXmCAam5FUEF9GN.4Z8OwwWJidvMii6VFYe77cmS2X6oF6p4W86', 1, 'Jaime', 'Agudo','jaimeagudovillanueva@gmail.com');
-INSERT INTO usuarios (username, password, enabled, nombre, apellido, email) VALUES ('admin','$2a$10$qGyDfZLBB.SgLv7GCP3uZe3oM38fVtr58T1iZ1LNOvO61loNUAAaK', 1, 'Cristina', 'Blanco','yavesyaveo@hotmail.com');
+INSERT INTO usuarios (username, password, enabled, nombre, apellido, email) VALUES ('jaime.agudo','$2a$10$ykhXmCAam5FUEF9GN.4Z8OwwWJidvMii6VFYe77cmS2X6oF6p4W86', true, 'Jaime', 'Agudo','jaimeagudovillanueva@gmail.com');
+INSERT INTO usuarios (username, password, enabled, nombre, apellido, email) VALUES ('admin','$2a$10$qGyDfZLBB.SgLv7GCP3uZe3oM38fVtr58T1iZ1LNOvO61loNUAAaK', true, 'Cristina', 'Blanco','yavesyaveo@hotmail.com');
 
 INSERT INTO roles (nombre) VALUES ('ROLE_USER');
 INSERT INTO roles (nombre) VALUES ('ROLE_ADMIN');
